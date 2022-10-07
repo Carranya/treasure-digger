124b0009b2bc6f760996fcdd9d80d78cd76d1125
Carranya <k.gong82@gmail.com>
2022-10-07 14:02:43 +0200

    new treasure.js

--- treasure.js
+++ treasure.js
@@ -1,5 +1,5 @@
-class Game{
-    constructor(){
+class Game {
+    constructor() {
         this.cols = 10;
         this.rows = 10;
         this.setChests = {};
@@ -7,7 +7,7 @@ class Game{
         this.turns = 0;
     }
 
-    board(){
+    board() {
         let boardWidth = this.cols * 50;
         let boardHeight = this.rows * 50;
 
@@ -22,44 +22,80 @@ class Game{
         document.getElementById("main").append(board);
     }
 
-    randChests(){
-        for(let i=0; i<this.chests; i++){
+    randChests() {
+        this.counter = 0;
+        for (let i = 0; i < this.chests; i++) {
             let randno = Math.floor(Math.random() * 100);
             let randId = randno.toString();
-            this.setChests[randId] = "1";
+            this.setChests[randId] = "chest";
         }
     }
 
-    show(){
-        for(let c=0; c<this.cols; c++){
-            for(let r=0; r<this.rows; r++){
-                let id = c + "" + r;
-                let text = "ID: " + id + " / Nr. " + this.setChests[id];
-                let test = document.createElement("div");
-                test.setAttribute("id", id);
-                test.innerHTML = text;
-                document.getElementById("test").append(test);
-            }
-        }
-    }
+    setTiles() {
+         this.setChests["56"] = "chest";
+        this.setChests["35"] = "chest";
+        this.setChests["23"] = "chest";
+        this.setChests["85"] = "chest";
+        this.setChests["46"] = "chest";
+        this.setChests["72"] = "chest";
+        this.setChests["77"] = "chest";
+        this.setChests["69"] = "chest";
+        this.setChests["12"] = "chest";
+        this.setChests["03"] = "chest"; 
 
-    check(){
-        for(let c=0; c<this.cols; c++){
-            for(let r=0; r<this.rows; r++){
-                let id = c + "" + r;
-                if(this.setChests[id] == "1"){
-                    alert(id);
+        for (let c = 0; c < this.cols; c++) {
+            for (let r = 0; r < this.rows; r++) {
+                let tileId = c + "" + r;
+                let tile = document.createElement("img");
+
+                if (this.setChests[tileId] == undefined) {
+                    this.setChests[tileId] = "empty";
                 }
+
+                if (this.setChests[tileId] == "chest") {
+                    // let checkId = parseInt("52");
+                    let checkId = parseInt(tileId);
+                    // let checkId = 51;
+                    let direction = [
+                        (checkId - 10),
+                        (checkId + 1),
+                        (checkId + 10),
+                        (checkId - 1)
+                    ];
+
+             
+                  
+
+                     for (let i = 0; i < 4; i++) {
+                        let nearTileId = direction[i].toString();
+                        if(this.setChests[nearTileId] != "chest"){
+                        this.setChests[nearTileId] = "near";
+                        }
+                    } 
+                }
+
+
+
+                tile.src = "img/" + this.setChests[tileId] + ".jpg";
+                tile.id = tileId;
+                tile.setAttribute("style", "");
+                tile.style.width = "48px";
+                tile.style.height = "48px";
+                tile.style.border = "1px solid black";
+                document.getElementById("board").append(tile);
             }
         }
+
+
     }
+
 }
 
-window.onload = function(){
+window.onload = function () {
     let game;
     game = new Game;
     game.board();
-    game.randChests();
-    game.show();
-    game.check();
+    // game.randChests();
+    game.setTiles();
+
 }
