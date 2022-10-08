d22281f1ec04de65255d9279ccd49e3bc06b16d4
Carranya <k.gong82@gmail.com>
2022-10-08 09:04:01 +0200

    fixed chests problem

--- treasure.js
+++ treasure.js
@@ -5,9 +5,10 @@ class Game {
         this.setChests = {};
         this.chests = 10;
         this.turns = 0;
+        this.counter = 0;
     }
 
-    board() {
+    createBoardSize() {
         let boardWidth = this.cols * 50;
         let boardHeight = this.rows * 50;
 
@@ -22,62 +23,73 @@ class Game {
         document.getElementById("main").append(board);
     }
 
-    randChests() {
+/*     randChests() {
         this.counter = 0;
         for (let i = 0; i < this.chests; i++) {
             let randno = Math.floor(Math.random() * 100);
             let randId = randno.toString();
             this.setChests[randId] = "chest";
         }
+    } */
+
+    randChests() {
+        let randCol = Math.floor(Math.random() * this.cols);
+        let randRow = Math.floor(Math.random() * this.rows);
+
+        if(this.setChests[randCol + "-" + randRow] == undefined){
+            this.setChests[randCol + "-" + randRow] = "chest";
+            this.counter++;
+        }
+
+        if(this.counter != this.chests){
+            this.randChests();
+        }
     }
 
     setTiles() {
-         this.setChests["56"] = "chest";
-        this.setChests["35"] = "chest";
-        this.setChests["23"] = "chest";
-        this.setChests["85"] = "chest";
-        this.setChests["46"] = "chest";
-        this.setChests["72"] = "chest";
-        this.setChests["77"] = "chest";
-        this.setChests["69"] = "chest";
-        this.setChests["12"] = "chest";
-        this.setChests["03"] = "chest"; 
-
         for (let c = 0; c < this.cols; c++) {
             for (let r = 0; r < this.rows; r++) {
                 let tileId = c + "" + r;
-                let tile = document.createElement("img");
+
+                if (this.setChests[tileId] == "chest") {
+                    let checkId = parseInt(tileId);
+                    let direction = [
+                        (checkId - this.cols),
+                        (checkId + 1),
+                        (checkId + this.cols),
+                        (checkId - 1)
+                    ];
+
+                    for (let i = 0; i < 4; i++) {
+                        let nearTileId
+                        if(direction[i] < 10){
+                            nearTileId = "0" + direction[i].toString();
+                        } else {
+                            nearTileId = direction[i].toString();
+                        }
+
+                        if (this.setChests[nearTileId] != "chest") {
+                            this.setChests[nearTileId] = "near";
+                        }
+                    }
+                }
 
                 if (this.setChests[tileId] == undefined) {
                     this.setChests[tileId] = "empty";
                 }
-
-                if (this.setChests[tileId] == "chest") {
-                    // let checkId = parseInt("52");
-                    let checkId = parseInt(tileId);
-                    // let checkId = 51;
-                    let direction = [
-                        (checkId - 10),
-                        (checkId + 1),
-                        (checkId + 10),
-                        (checkId - 1)
-                    ];
-
-             
-                  
-
-                     for (let i = 0; i < 4; i++) {
-                        let nearTileId = direction[i].toString();
-                        if(this.setChests[nearTileId] != "chest"){
-                        this.setChests[nearTileId] = "near";
-                        }
-                    } 
-                }
+            }
+        }
 
 
+    }
 
-                tile.src = "img/" + this.setChests[tileId] + ".jpg";
-                tile.id = tileId;
+    createBoard() {
+        for (let c = 0; c < this.cols; c++) {
+            for (let r = 0; r < this.rows; r++) {
+                let fieldId = c + "" + r;
+                let tile = document.createElement("img");
+                tile.src = "img/" + this.setChests[fieldId] + ".jpg";
+                tile.id = fieldId;
                 tile.setAttribute("style", "");
                 tile.style.width = "48px";
                 tile.style.height = "48px";
@@ -85,8 +97,6 @@ class Game {
                 document.getElementById("board").append(tile);
             }
         }
-
-
     }
 
 }
@@ -94,8 +104,9 @@ class Game {
 window.onload = function () {
     let game;
     game = new Game;
-    game.board();
-    // game.randChests();
+    game.createBoardSize();
+    game.randChests();
     game.setTiles();
+    game.createBoard();
 
 }
