7207827edb600b819f02dd4e25e35c6df80b8f23
Carranya <k.gong82@gmail.com>
2022-10-14 08:31:23 +0200

    add new styles

--- index.php
+++ index.php
@@ -16,38 +16,47 @@
         <div id="turns" class="infoScore"></div>
         <div id="findChests" class="infoScore"></div>
 
-    <div id="gameSettings" class="infoBox">
-        <p>Columns: <select id="setupCols">
-            <?php
-                for($c = 10; $c<=20; $c=$c+5){
-                    echo "<option value=$c>$c</option>";
-                }
-            ?>
-            </select><br>
+        <div id="footer">
+            <div id="footText">Karin Giang 2022</div>
+        </div>
+    <div class="idBox">
+        <div id="gameSettings" class="infoBox">
+            <p>Columns: <select id="setupCols">
+                <?php
+                    for($c = 10; $c<=20; $c=$c+5){
+                        echo "<option value=$c>$c</option>";
+                    }
+                ?>
+                </select><br>
 
-            Rows: <select id="setupRows">
-            <?php
-                for($r=10; $r<=20; $r=$r+5){
-                    echo "<option value=$r>$r</option>";
-                }
-            ?>
-            </select><br>
+                Rows: <select id="setupRows">
+                <?php
+                    for($r=10; $r<=20; $r=$r+5){
+                        echo "<option value=$r>$r</option>";
+                    }
+                ?>
+                </select><br>
 
-            Chests: <select id="setupChests">
-            <?php
-                for($ch=10; $ch<=20; $ch++){
-                    echo "<option value=$ch>$ch</option>";
-                }
-            ?>
-            </select></p>
-            
-        <p><input type="button" id="idSettings" value="Start Game"></p>
+                Chests: <select id="setupChests">
+                <?php
+                    for($ch=10; $ch<=20; $ch++){
+                        echo "<option value=$ch>$ch</option>";
+                    }
+                ?>
+                </select></p>
+                
+            <p><input type="button" id="idSettings" value="Start Game"></p>
+        </div>
+    </div> 
+
+    <div class="idBox">
+        <div id="winBox" class="infoBox">
+            <h1>You win!</h1>
+            <p>Turns: <span id="winTurns"></span></p>
+            <p><a href="index.php" id="newGame">New Game?</a><p>
+        </div>
     </div>
 
-    <div id="winBox" class="infoBox">
-        <h1>You win!</h1>
-        <p>Turns: <span id="winTurns"></span></p>
-        <p><a href="index.php" id="newGame">New Game?</a><p>
-    </div>
+    
 </body>
 </html>
